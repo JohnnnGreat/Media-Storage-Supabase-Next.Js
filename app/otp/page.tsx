8d8797bd471d843f154ca6b88761d8e4b3f7a1eb
John Ossai <johnossai20@gmail.com>
2024-05-17 13:17:35 +0100

    check new Errir

--- app/otp/page.tsx
+++ app/otp/page.tsx
@@ -11,6 +11,7 @@ import { useRouter } from "next/navigation";
 import { customToastNotifier } from "@/utils/shared";
 import { useSignUpUser, useVerifyOtp } from "@/utils/tanstack/tanstackQueries";
 import { message } from "antd";
+import { Suspense } from "react";
 
 const Otp = () => {
 	const [value, setValue] = React.useState<string>("");
@@ -84,57 +85,59 @@ const Otp = () => {
 	};
 
 	return (
-		<div className="h-screen w-full flex items-center justify-center">
-			<div className="absolute top-0 w-[1200px] mx-auto flex justify-between items-center h-[80px]">
-				<Link href="/" className="flex items-center justify-center gap-2 p-[.5rem] px-[.8rem] hover:bg-slate-100 transition-all rounded-md">
-					<ArrowLeft size={16} />
-					<p className="text-[.85rem]">Home</p>
-				</Link>
-				<Link href="/login" className="flex items-center justify-center gap-2 p-[.5rem] px-[.8rem] hover:bg-slate-100 transition-all rounded-md">
-					<p className="text-[.85rem]">Login</p>
-					<ArrowRight size={16} />
-				</Link>
+		<Suspense fallback="..loading">
+			<div className="h-screen w-full flex items-center justify-center">
+				<div className="absolute top-0 w-[1200px] mx-auto flex justify-between items-center h-[80px]">
+					<Link href="/" className="flex items-center justify-center gap-2 p-[.5rem] px-[.8rem] hover:bg-slate-100 transition-all rounded-md">
+						<ArrowLeft size={16} />
+						<p className="text-[.85rem]">Home</p>
+					</Link>
+					<Link href="/login" className="flex items-center justify-center gap-2 p-[.5rem] px-[.8rem] hover:bg-slate-100 transition-all rounded-md">
+						<p className="text-[.85rem]">Login</p>
+						<ArrowRight size={16} />
+					</Link>
+				</div>
+				<div className="flex flex-col items-center">
+					<h1 className="text-[1.8rem] font-bold my-[.5rem]"> We've sent you a code!</h1>
+					<p className="text-gray-500 text-[.8rem] mb-[.5rem]">Open your email and copy-paste the code here.</p>
+					<InputOTP onChange={(value: any) => setValue(value)} maxLength={6}>
+						<InputOTPGroup>
+							<InputOTPSlot index={0} className="h-[60px]" />
+							<InputOTPSlot index={1} className="h-[60px]" />
+							<InputOTPSlot index={2} className="h-[60px]" />
+						</InputOTPGroup>
+						<InputOTPSeparator />
+						<InputOTPGroup>
+							<InputOTPSlot index={3} className="h-[60px]" />
+							<InputOTPSlot index={4} className="h-[60px]" />
+							<InputOTPSlot index={5} className="h-[60px]" />
+						</InputOTPGroup>
+					</InputOTP>
+					<Button disabled={value.length < 6 && true} className="bg-blue-500 w-full text-white mt-[.7rem]" onClick={verifyOtp}>
+						{isPending ? (
+							<div className="flex gap-2">
+								<Loader2 className="mr-2 h-4 w-4 animate-spin" /> Veriying, Please wait...
+							</div>
+						) : (
+							<>Verify</>
+						)}
+					</Button>
+					<Button className="bg-gray-300 w-[full] mt-[1rem] text-gray-600 flex items-center justify-center gap-2" onClick={resendOtp}>
+						{isLoading ? (
+							<>
+								<RefreshCcw className="animate-spin" size={16} />
+								<span>Sending OTP...</span>
+							</>
+						) : (
+							<div className="flex gap-2">
+								<RefreshCcw size={16} />
+								<span>Resend OTP ({resendCount})</span>
+							</div>
+						)}
+					</Button>
+				</div>
 			</div>
-			<div className="flex flex-col items-center">
-				<h1 className="text-[1.8rem] font-bold my-[.5rem]"> We've sent you a code!</h1>
-				<p className="text-gray-500 text-[.8rem] mb-[.5rem]">Open your email and copy-paste the code here.</p>
-				<InputOTP onChange={(value: any) => setValue(value)} maxLength={6}>
-					<InputOTPGroup>
-						<InputOTPSlot index={0} className="h-[60px]" />
-						<InputOTPSlot index={1} className="h-[60px]" />
-						<InputOTPSlot index={2} className="h-[60px]" />
-					</InputOTPGroup>
-					<InputOTPSeparator />
-					<InputOTPGroup>
-						<InputOTPSlot index={3} className="h-[60px]" />
-						<InputOTPSlot index={4} className="h-[60px]" />
-						<InputOTPSlot index={5} className="h-[60px]" />
-					</InputOTPGroup>
-				</InputOTP>
-				<Button disabled={value.length < 6 && true} className="bg-blue-500 w-full text-white mt-[.7rem]" onClick={verifyOtp}>
-					{isPending ? (
-						<div className="flex gap-2">
-							<Loader2 className="mr-2 h-4 w-4 animate-spin" /> Veriying, Please wait...
-						</div>
-					) : (
-						<>Verify</>
-					)}
-				</Button>
-				<Button className="bg-gray-300 w-[full] mt-[1rem] text-gray-600 flex items-center justify-center gap-2" onClick={resendOtp}>
-					{isLoading ? (
-						<>
-							<RefreshCcw className="animate-spin" size={16}  />
-							<span>Sending OTP...</span>
-						</>
-					) : (
-						<div className="flex gap-2">
-							<RefreshCcw size={16} />
-							<span>Resend OTP ({resendCount})</span>
-						</div>
-					)}
-				</Button>
-			</div>
-		</div>
+		</Suspense>
 	);
 };
 
